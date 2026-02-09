04c911438bdf2495796f098c8f367d0efa7d5435
Ahmet Karabulut <a.karabulut070@gmail.com>
2026-02-09 16:00:02 +0100

    feat: create reusable SearchBar with custom styling

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -21,6 +21,7 @@
 </template>
 
 <script setup>
+import {ref} from 'vue';
 
 //local storage
 const villeSaisie=ref('');
@@ -42,6 +43,48 @@ const emttreRecerche = ()=>{
     margin : 20 px 0;
 
 }
+
+.champ-saisie-groupe {
+    display: flex;
+    align-items: center;
+    border: 3px solid black;
+    background-color: white;
+    padding: 0;
+    height: 60px;
+}
+
+.icone-loupe {
+    padding: 0 15px;
+    font-size: 1.2rem;
+}
+
+input {
+    flex-grow: 1;
+    border: none;
+    outline: none;
+    font-size:  1.1rem;
+    font-family: Arial, sans-serif;
+}
+
+.bouton-chercher {
+    background-color: black;
+    color: white;
+    border: none;
+    height: 100%;
+    padding: 0 30px;
+    cursor: pointer;
+    font-weight: bold;
+    font-size: 1rem;
+    transition: black 0.2s;
+
+}
+
+.bouton-chercher:hover {
+    background-color: #333;
+}
+
+
+
 
 
 
