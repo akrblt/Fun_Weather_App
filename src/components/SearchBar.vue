b4c5390aed1278dca74de30734768a719baa6e77
Ahmet Karabulut <a.karabulut070@gmail.com>
2026-02-09 14:38:36 +0100

    feat : search bar component

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,49 @@
+
+
+
+<template>
+    <div class="barre-recherche-container">
+        <div class="champ-saisie-groupe">
+            <span class="icone-loupe">🔍</span>
+            <input 
+            type="text"
+            v-model="villeSaisie"
+            placeholder="Entrez le nom de la ville... "
+            @keyup.enter="emttreRecerche"
+            />
+            <button @click="emttreRecerche" class="bouton-chercher" >
+                CHERCHER
+            </button>
+        </div>
+    </div>
+
+
+</template>
+
+<script setup>
+
+//local storage
+const villeSaisie=ref('');
+
+// emit enfant vers parent
+const emit =defineEmits(['chercher']);
+
+const emttreRecerche = ()=>{
+    if (villeSaisie.value.trim() !== ''){
+        emit('chercher',villeSaisie.value)
+    }
+};
+
+</script>
+
+<style scoped>
+
+.barre-recherche-container {
+    margin : 20 px 0;
+
+}
+
+
+
+
+</style>
