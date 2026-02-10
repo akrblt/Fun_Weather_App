328ff74914d99e0110350454fdfae0762e65135f
Ahmet Karabulut <a.karabulut070@gmail.com>
2026-02-10 12:24:51 +0100

    chore: configure vitest environment with jsdom in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  test: {
+    // c'est neccessire que jsdom pour vitest  
+    environment: 'jsdom',
+    globals: true, // fonction describe,it etc etre global 
+  }
 })
