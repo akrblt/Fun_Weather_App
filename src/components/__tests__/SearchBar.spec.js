2689d2542a015c1ac3fa0e334cf75f5ac4473920
Ahmet Karabulut <a.karabulut070@gmail.com>
2026-02-10 14:20:21 +0100

    feat: test: implement initial unit test and create first test for SearchBar component

---
+++ src/components/__tests__/SearchBar.spec.js
@@ -0,0 +1,16 @@
+import { mount } from '@vue/test-utils'
+import { describe, it, expect } from 'vitest'
+import SearchBar from '../SearchBar.vue' // be careful the path is correct
+
+describe('SearchBar Test', () => {
+  it('should have a button with the text CHERCHER', () => {
+    // Bileşeni sanal olarak oluştur
+    const wrapper = mount(SearchBar)
+    
+    // Find the button element
+    const buton = wrapper.find('button')
+    
+    // Assert that the button text is "CHERCHER"
+    expect(buton.text()).toBe('CHERCHER')
+  })
+})
