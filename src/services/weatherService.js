3a9bc04345fceba9c13e026607f372bc7eeeaaf5
Ahmet Karabulut <a.karabulut070@gmail.com>
2026-02-24 15:15:34 +0100

    feat: add forecast service to fetch multi-day weather data

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -2,11 +2,12 @@ import axios from 'axios';
 
 // take a apikey from .env
 const API_KEY=import.meta.env.VITE_WEATHER_API_KEY;
-const BASE_URL='https://api.openweathermap.org/data/2.5/weather';
+// ce lien permet de recuperer de donnes via Api
+const BASE_URL='https://api.openweathermap.org/data/2.5';
 
 export async function getWeatherByCity(city){
     try {
-        const response = await axios.get(BASE_URL, {
+        const response = await axios.get(`${BASE_URL}/weather`, {
             params:{
                 q: city,
                 appid: API_KEY,
@@ -21,6 +22,25 @@ export async function getWeatherByCity(city){
         throw error
     }
     
-    
 
 }
+
+ // 5 jours de donnes avec la ville
+
+ export async function getForeCastByCity(city) {
+    try {
+        const response = await axios.get(`${BASE_URL}/forecast`,{
+            params: {
+                q: city,
+                appid: API_KEY,
+                units: 'metric',
+                lang: 'fr'
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.error("Erreur API météo (Forecat): ",error)
+        throw error;
+    }
+    
+ }
