528d3b0f97bdb08f79a3cb2be32e0fb377a7e146
Ahmet Karabulut <a.karabulut070@gmail.com>
2026-03-23 16:00:27 +0100

    fix: merge conflit resolved

--- src/components/scenes/WindyScene.vue
+++ src/components/scenes/WindyScene.vue
@@ -1,57 +1,55 @@
 <script setup>
-import vue3lottie, { Vue3Lottie } from 'vue3-lottie';
+import { Vue3Lottie } from 'vue3-lottie';
 import windy_flag from '../../assets/animations/windy_flag.json'
 import windy_person from '../../assets/animations/windy_person.json'
 import windy_souffle from '../../assets/animations/windy_souffle.json'
 </script>
 
 <template>
-
-<div class="lottie-canvas windy">
+  <div class="lottie-canvas windy">
     <div class="layer wind">
-        <Vue3Lottie :animation-data="windy_souffle" :height="130" width="80"  />
-        <Vue3Lottie :animation-data="windy_souffle" :height="130" width="80" />
-        <Vue3Lottie :animation-data="windy_souffle" :height="130" width="80"  />
+      <Vue3Lottie :animation-data="windy_souffle" :height="130" :width="80" />
+      <Vue3Lottie :animation-data="windy_souffle" :height="130" :width="80" />
+      <Vue3Lottie :animation-data="windy_souffle" :height="130" :width="80" />
     </div>
     <div class="layer person">
-        <Vue3Lottie :animation-data="windy_person" :height="150" width="80" speed="1.7" />
-        
+      <Vue3Lottie :animation-data="windy_person" :height="150" :width="80" speed="1.7" />
     </div>
     <div class="layer person1">
-        <Vue3Lottie :animation-data="windy_person" :height="150" width="80" speed="1.7" />
-       
+      <Vue3Lottie :animation-data="windy_person" :height="150" :width="80" speed="1.7" />
     </div>
     <div class="layer flag">
-        <Vue3Lottie :animation-data="windy_flag" :width="130" height="80"/>
-        <Vue3Lottie :animation-data="windy_flag" :width="130" height="80"/>
-        <Vue3Lottie :animation-data="windy_flag" :width="130" height="80"/>
+      <Vue3Lottie :animation-data="windy_flag" :width="130" :height="80" />
+      <Vue3Lottie :animation-data="windy_flag" :width="130" :height="80" />
+      <Vue3Lottie :animation-data="windy_flag" :width="130" :height="80" />
     </div>
-</div>
-
+  </div>
 </template>
 
 <style scoped>
 .lottie-canvas {
-    position: absolute; inset: 0; pointer-events: none;
+  position: absolute;
+  inset: 0;
+  pointer-events: none;
 }
-
 .layer {
-    position: absolute;
+  position: absolute;
 }
-.wind{
-top: 50px; left: 0;
+.wind {
+  top: 50px;
+  left: 0;
 }
 .person {
-    top: 60%; left: 25%;
-
+  top: 60%;
+  left: 25%;
 }
 .person1 {
-    top: 25%; left: 57%;
-
+  top: 25%;
+  left: 57%;
 }
 .flag {
-    top: 40px; left: 90%; transform: translateX(-60%);
-
+  top: 40px;
+  left: 90%;
+  transform: translateX(-60%);
 }
-
 </style>
